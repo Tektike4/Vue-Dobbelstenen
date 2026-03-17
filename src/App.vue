85b70fc2a9132282ebca06d831cf73708c15e3ef
Tektike4 <buijinktom@gmail.com>
2026-03-17 16:05:28 +0100

    boop

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <button @click="throwdice()">Gooi de dobbelstenen</button>
+    <br />
+    <ul>
+        <li v-for="(diceTotal, index) in dice" :key="index">{{ index }} : {{ diceTotal }}</li>
+    </ul>
+</template>
+<script setup>
+import {reactive} from 'vue';
+const dice = reactive({
+    1: 0,
+    2: 0,
+    3: 0,
+    4: 0,
+    5: 0,
+    6: 0,
+});
+
+const throwdice = () => {
+    for (let i = 0; i < 8; i++) {
+        let randomNumber = Math.floor(Math.random() * 6 + 1);
+        dice[randomNumber] += 1;
+    }
+    console.log(dice);
+};
+</script>
+
+<style scoped></style>
